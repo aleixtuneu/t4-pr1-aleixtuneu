39b82265269ac315fb22d78de8847ae57c3c133f
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-04 21:32:16 +0100

    feature: added energy simulations tests

---
+++ T4.PR1/T4.Pr1.Tests/EnergySimulationsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using T4.PR1.Model;
+
+namespace T4.PR1.Tests
+{
+    [TestClass]
+    public class EnergySimulationTests
+    {
+        [TestMethod]
+        public void CalculateEnergy_SolarSystem_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            decimal inputValue = 100;
+            decimal ratio = 1.5m;
+            decimal costPerKWh = 0.10m;
+            decimal pricePerKWh = 0.20m;
+
+            // Act
+            var simulation = new EnergySimulation("Solar", inputValue, ratio, costPerKWh, pricePerKWh);
+
+            // Assert
+            Assert.AreEqual(inputValue * ratio, simulation.EnergyGenerated, "L'energia generada no és correcta.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_InvalidSystemType_ShouldThrowException()
+        {
+            // Act
+            var simulation = new EnergySimulation("InvalidType", 100, 1.5m, 0.1m, 0.2m);
+        }
+    }
+}
---
+++ T4.PR1/T4.Pr1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("T4.Pr1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T4.Pr1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8826ae60-e019-4dbc-86fc-440a4fc43cc5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
