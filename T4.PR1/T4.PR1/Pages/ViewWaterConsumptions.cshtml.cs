2ef68c7c4b7fe720545b994f984e0642f56aabd2
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-04 12:53:15 +0100

    feature: show water consumptions statistical analysis

--- T4.PR1/T4.PR1/Pages/ViewWaterConsumptions.cshtml.cs
+++ T4.PR1/T4.PR1/Pages/ViewWaterConsumptions.cshtml.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using T4.PR1.Model;
-using System.Security.Cryptography.X509Certificates;
-using FileWorking = System.IO;
-using System.Diagnostics;
 using System.Globalization;
 using CsvHelper;
-using CsvHelper.Configuration;
-using System;
+using System.Xml.Linq;
+
 
 namespace T4.PR1.Pages
 {
@@ -17,7 +14,12 @@ namespace T4.PR1.Pages
 
         public List<WaterConsumption> WaterConsumptions { get; set; } = new List<WaterConsumption>();
 
-        public void OnGet()
+		public List<WaterConsumption> TopTenMunicipalities { get; set; } = new List<WaterConsumption>();
+		public Dictionary<string, decimal> AvgConsumptionPerCounty { get; set; } = new();
+		public List<WaterConsumption> SuspiciousConsumptions { get; set; } = new List<WaterConsumption>();
+		public List<string> GrowingConsumptionMunicipalities { get; set; } = new List<string>();
+
+		public void OnGet()
         {
 			string filePath = @"ModelData\consum_aigua_cat_per_comarques.csv";
 
@@ -51,6 +53,41 @@ namespace T4.PR1.Pages
 						{
 							FileErrorMessage = "Error al carregar als atributs d'un consum d'aigua";
 						}
+					}
+
+					if (WaterConsumptions.Count > 0)
+					{
+						var latestYear = WaterConsumptions.Max(x => x.Year);
+
+						// Top 10 municipis amb més consum en l'últim any
+						TopTenMunicipalities = WaterConsumptions
+							.Where(x => x.Year == latestYear)
+							.OrderByDescending(x => x.TotalWaterConsumption)
+							.Take(10)
+							.ToList();
+
+						// Consum mig per comarca
+						AvgConsumptionPerCounty = WaterConsumptions
+							.GroupBy(x => x.County)
+							.ToDictionary(
+								g => g.Key, 
+								g => (decimal)g.Average(x => x.TotalWaterConsumption));
+
+                        // Consums sospitosos (majors a 1000000 o iguals)
+                        SuspiciousConsumptions = WaterConsumptions
+							.Where(x => x.TotalWaterConsumption >= 1000000)
+							.ToList();
+
+                        // Municipis amb consum creixent en els últims 5 anys
+                        GrowingConsumptionMunicipalities = WaterConsumptions
+							.GroupBy(x => x.County)
+							.Where(g =>
+							{
+								var last5Years = g.OrderByDescending(x => x.Year).Take(5).Select(x => x.TotalWaterConsumption).ToList();
+								return last5Years.Zip(last5Years.Skip(1), (a, b) => a > b).All(increasing => increasing);
+							})
+							.Select(g => g.Key)
+							.ToList();
 					}
 				}
 			}
