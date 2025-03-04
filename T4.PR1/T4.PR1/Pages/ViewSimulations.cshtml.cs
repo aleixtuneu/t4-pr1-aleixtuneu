4de8a0a022c8d8411a30afe28fed498c91850cbe
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-03-04 20:15:15 +0100

    feature: view simulations report, add new simulation button

---
+++ T4.PR1/T4.PR1/Pages/ViewSimulations.cshtml.cs
@@ -0,0 +1,56 @@
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using T4.PR1.Model;
+
+namespace T4.PR1.Pages
+{
+    public class ViewSimulationsModel : PageModel
+    {
+        public string FileErrorMessage;
+
+        public List<EnergySimulation> Simulations { get; set; } = new List<EnergySimulation>();
+        
+        public void OnGet()
+        {
+            string filePath = @"ModelData\simulacions_energia.csv";
+
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+
+                    while (csv.Read())
+                    {
+                        try
+                        {
+                            var energySimulation = new EnergySimulation
+                            {
+                                SimulationDate = csv.GetField<DateTime>(0),
+                                SystemType = csv.GetField<string>(1),
+                                InputValue = csv.GetField<decimal>(2),
+                                Ratio = csv.GetField<decimal>(3),
+                                EnergyGenerated = csv.GetField<decimal>(4),
+                                CostPerKWh = csv.GetField<decimal>(5),
+                                PricePerKWh = csv.GetField<decimal>(6)
+                            };
+                            Simulations.Add(energySimulation);
+                        }
+                        catch
+                        {
+                            FileErrorMessage = "Error en llegir una línia del fitxer.";
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                FileErrorMessage = "Error de càrrega de dades.";
+            }
+        }
+    }
+}
